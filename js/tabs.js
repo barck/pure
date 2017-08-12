e57ccf5f017e997934de1aebad8fb9a9080b76d8
Dilan Mckey <mckey@macbook-2.local>
2017-08-12 18:37:15 +0500

    tabs review

--- js/tabs.js
+++ js/tabs.js
@@ -1,30 +1,31 @@
-var link1 = document.querySelector(".tab1_link");
-var link2 = document.querySelector(".tab2_link");
-var link3 = document.querySelector(".tab3_link");
-var tab1_box = document.querySelector(".tab1");
-var tab2_box = document.querySelector(".tab2");
-var tab3_box = document.querySelector(".tab3");
+var link1 = document.querySelector(".tab2_link");
+// var link2 = document.querySelector(".tab2_link");
+// var link3 = document.querySelector(".tab3_link");
+var tab1_box = document.querySelector(".tab_box");
+// var tab2_box = document.querySelector(".tab2");
+// var tab3_box = document.querySelector(".tab3");
 
 
 link1.addEventListener("click", function(event){
 	event.preventDefault();
-	tab2_box.classList.remove("visible");
-	tab3_box.classList.remove("visible");
-	tab1_box.classList.add("visible");
+	
+	// tab2_box.classList.remove("visible");
+	// tab3_box.classList.remove("visible");
+	// tab1_box.classList.add("visible");
 });
 
 
-link2.addEventListener("click", function(event){
-	event.preventDefault();
-	tab1_box.classList.remove("visible");
-	tab3_box.classList.remove("visible");
-	tab2_box.classList.add("visible");
-});
+// link2.addEventListener("click", function(event){
+// 	event.preventDefault();
+// 	tab1_box.classList.remove("visible");
+// 	tab3_box.classList.remove("visible");
+// 	tab2_box.classList.add("visible");
+// });
 
 
-link3.addEventListener("click", function(event){
-	event.preventDefault();
-	tab1_box.classList.remove("visible");
-	tab2_box.classList.remove("visible");
-	tab3_box.classList.add("visible");
-});
+// link3.addEventListener("click", function(event){
+// 	event.preventDefault();
+// 	tab1_box.classList.remove("visible");
+// 	tab2_box.classList.remove("visible");
+// 	tab3_box.classList.add("visible");
+// });
