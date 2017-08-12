1845c122cfd440fa47c79c490411c21be9c06c5a
barck94 <artiv09@gmail.com>
2017-08-12 22:20:54 +0500

    поломал
    Теперь все поломано

--- js/tabs.js
+++ js/tabs.js
@@ -1,31 +1,9 @@
-var link1 = document.querySelector(".tab2_link");
-// var link2 = document.querySelector(".tab2_link");
-// var link3 = document.querySelector(".tab3_link");
-var tab1_box = document.querySelector(".tab_box");
-// var tab2_box = document.querySelector(".tab2");
-// var tab3_box = document.querySelector(".tab3");
+var link = document.querySelectorAll(".tab_link");
 
-
-link1.addEventListener("click", function(event){
-	event.preventDefault();
+link.addEventListener("click", function(event){
 	
-	// tab2_box.classList.remove("visible");
-	// tab3_box.classList.remove("visible");
-	// tab1_box.classList.add("visible");
+	event.preventDefault();
+	console.log(event);
+
 });
 
-
-// link2.addEventListener("click", function(event){
-// 	event.preventDefault();
-// 	tab1_box.classList.remove("visible");
-// 	tab3_box.classList.remove("visible");
-// 	tab2_box.classList.add("visible");
-// });
-
-
-// link3.addEventListener("click", function(event){
-// 	event.preventDefault();
-// 	tab1_box.classList.remove("visible");
-// 	tab2_box.classList.remove("visible");
-// 	tab3_box.classList.add("visible");
-// });
